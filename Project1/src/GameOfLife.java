c485bc94ce8f3d5dcb10870439660b540368bb13
Joshua Benz <joshua@planethull.com>
2019-05-25 22:08:43 -0700

    render world

--- Project1/src/GameOfLife.java
+++ Project1/src/GameOfLife.java
@@ -1,6 +1,10 @@
+import java.util.Random;
+
 public class GameOfLife {
     static int[][] world;
     static boolean isRunning;
+    final static int width = 10;
+    final static int height = 10;
 
     public static void epoch() {
 
@@ -10,19 +14,34 @@ public class GameOfLife {
         return 0;
     }
 
-    public static void randomSeed() {
+    public static int[][] randomSeed() {
+        int[][] tmp = new int[width][height];
+        Random rand = new Random();
 
+        for(int i=0; i<tmp.length; i++) {
+            for(int j=0; j<tmp[i].length; j++) {
+                tmp[i][j] = rand.nextInt(2);
+            }
+        }
+        return tmp;
     }
 
     public static void run() {
 
     }
 
-    public static void render() {
-
+    public static void render(int[][] grid) {
+        for(int i=0; i<grid.length; i++) {
+            for(int j=0; j<grid[i].length; j++) {
+                System.out.print(grid[i][j] + "  ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println("helloo");
+        world = randomSeed();
+        render(world);
+
     }
 }
