5b1a5a889bf064d8e99a025dead91f61d8667563
Joshua Benz <joshua@planethull.com>
2019-05-25 21:44:18 -0700

    skeleton

--- Project1/src/GameOfLife.java
+++ Project1/src/GameOfLife.java
@@ -1,4 +1,28 @@
 public class GameOfLife {
     static int[][] world;
     static boolean isRunning;
+
+    public static void epoch() {
+
+    }
+
+    public static int fitness(int x, int y) {
+        return 0;
+    }
+
+    public static void randomSeed() {
+
+    }
+
+    public static void run() {
+
+    }
+
+    public static void render() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("helloo");
+    }
 }
